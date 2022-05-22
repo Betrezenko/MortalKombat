854261ba07b718331c367aac66c7c739df9010b7
Viktor.Igumenov <betrezenko@gmail.com>
2022-05-22 22:42:41 +0300

    Homework 3
    Add random kick.
    Add winner banner.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
 
 const player1 = {
     player: 1,
     name: 'Vasya',
-    hp: 80,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['bow', 'sword', 'staff', 'dagger'],
     attack: function () {
@@ -16,7 +17,7 @@ const player1 = {
 const player2 = {
     player: 2,
     name: 'Petya',
-    hp: 50,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['bow', 'sword', 'staff', 'dagger'],
     attack: function () {
@@ -52,6 +53,46 @@ function createPlayer(objPlayer) {
     $player.appendChild($character);
     return $player;
 }
+
+function changeHP(player){
+    const $playerLife = document.querySelector('.player' + player.player + ' .life');
+    
+    console.log('1. ' + player1.hp + ' 2. ' + player2.hp);
+    
+    let $kick = Math.ceil(Math.random() * 20);
+    
+    console.log($kick);
+    
+    if (player.hp - $kick > 0) {
+        player.hp -= $kick;
+    } else {
+        function whoWon() {
+            if (player1.hp > 0) {
+                $arenas.appendChild(playerLose(player1.name));
+            } else {
+                $$arenas.appendChild(playerLose(player2.name));
+            }
+        }
+        whoWon();
+        player.hp = 0;
+        $randomButton.disabled = true;
+    }
+    
+    console.log('1. ' + player1.hp + ' 2. ' + player2.hp);
+    
+    $playerLife.style.width = player.hp + '%';
+}
+
+function playerLose(name) {
+    const $loseTitle = createElement('div', 'loseTitle');
+    $loseTitle.innerText = name + ' wins';
+    return $loseTitle;
+}
+
+$randomButton.addEventListener('click', function() {
+    changeHP(player1);
+    changeHP(player2);
+});
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
