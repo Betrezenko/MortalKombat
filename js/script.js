ee765fbe0f6b4f8a1de370feabdb5648e8ff0a15
Viktor.Igumenov <betrezenko@gmail.com>
2022-05-24 00:15:34 +0300

    Add reload button

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ const player1 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['bow', 'sword', 'staff', 'dagger'],
-    attack: function () {
-        console.log(`${player1.name}` + ' Fight...');
-    }
+    attack: attack,
+    changeHP: changeHP,
+    elHP: elHP,
+    renderHP: renderHP
 };
 
 const player2 = {
@@ -20,10 +21,30 @@ const player2 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['bow', 'sword', 'staff', 'dagger'],
-    attack: function () {
-        console.log(`${player2.name}` + ' Fight...');
-    }
+    attack: attack,
+    changeHP: changeHP,
+    elHP: elHP,
+    renderHP: renderHP
 };
+
+function attack() {
+    console.log(this.name + ' Fight...');
+}
+
+function elHP(){
+    return document.querySelector('.player' + this.player + ' .life');
+}
+
+function renderHP() {
+    this.elHP().style.width = this.hp + '%';
+}
+
+function changeHP($kick){
+    this.hp -= $kick;
+    if (this.hp <= 0) {
+        this.hp = 0;
+    }
+}
 
 function createElement(tag, className){
     const $tag = document.createElement(tag);
@@ -54,46 +75,56 @@ function createPlayer(objPlayer) {
     return $player;
 }
 
-function whoWon() {
-    if (player1.hp <= 0) {
-        $arenas.appendChild(playerLose(player2.name));
-    } else {
-        $arenas.appendChild(playerLose(player1.name));
-    }
-    $randomButton.disabled = true;
+function getRandom(num) {
+    let $kick = Math.ceil(Math.random() * num);
+    return $kick;
 }
 
-function changeHP(player){
-    const $playerLife = document.querySelector('.player' + player.player + ' .life');
-    
-    console.log('1. ' + player1.hp + ' 2. ' + player2.hp);
-    
-    let $kick = Math.ceil(Math.random() * 20);
-    
-    console.log($kick);
-    
-    if (player.hp - $kick > 0) {
-        player.hp -= $kick;
-    } else {
-        whoWon();
-        player.hp = 0;
-        
-    }
-    
-    console.log('1. ' + player1.hp + ' 2. ' + player2.hp);
-    
-    $playerLife.style.width = player.hp + '%';
-}
-
-function playerLose(name) {
+function playerWin(name) {
     const $loseTitle = createElement('div', 'loseTitle');
-    $loseTitle.innerText = name + ' wins';
+    if (name) {
+        $loseTitle.innerText = name + ' wins';
+    } else {
+        $loseTitle.innerText = 'Draw';
+    }
+    
     return $loseTitle;
 }
 
+function createReloadButton(){
+    const $reloadDiv = createElement('div', 'reloadWrap'),
+          $reloadBtn = createElement('button', 'button');
+    
+    $reloadDiv.appendChild($reloadBtn);
+    $reloadBtn.innerText = 'Reload';
+
+    return $reloadDiv;
+}
+
 $randomButton.addEventListener('click', function() {
-    changeHP(player1);
-    changeHP(player2);
+    player1.changeHP(getRandom(20));
+    player1.renderHP();
+    player2.changeHP(getRandom(20));
+    player2.renderHP();
+
+    if (player1.hp === 0 || player2.hp === 0) {
+        $randomButton.disabled = true;
+        const $reloadButton = createReloadButton();
+        
+        $reloadButton.addEventListener('click', function() {
+            window.location.reload();
+        });
+    
+        $arenas.appendChild($reloadButton);
+    }
+
+    if (player1.hp === 0 && player1.hp < player2.hp) {
+        $arenas.appendChild(playerWin(player2.name));
+    } else if (player2.hp === 0 && player2.hp < player1.hp) {
+        $arenas.appendChild(playerWin(player1.name));
+    } else if (player1.hp === 0 && player2.hp === 0) {
+        $arenas.appendChild(playerWin());
+    }
 });
 
 $arenas.appendChild(createPlayer(player1));
